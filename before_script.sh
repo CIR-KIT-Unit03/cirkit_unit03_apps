fca40ccecea9b6d0e286b920210a59f201425036
MoriKen254 <masamorobo@gmail.com>
2016-12-18 22:15:02 +0900

    Modify before script to make all pkgs.

--- before_script.sh
+++ before_script.sh
@@ -2,3 +2,8 @@
 pwd
 apt-get install -y software-properties-common python-software-properties
 yes | apt-add-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl; apt-get update -qq; apt-get install -y libpcl-all;
+
+rosdep install -i -r -y --from-paths . --ignore-src --rosdistro indigo
+cd ..
+catkin_make
+cd src
