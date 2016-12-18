bfe874cab83826b58485c37a8c51204e7706ef4c
MoriKen254 <masamorobo@gmail.com>
2016-12-18 21:23:28 +0900

    Fix before_script for apt-add-repo

--- before_script.sh
+++ before_script.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+sudo apt-get install -y software-properties-common python-software-properties
 yes | sudo apt-add-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl; sudo apt-get update -qq; sudo apt-get install -y libpcl-all;
