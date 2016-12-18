83893c487020ec719bf934fc5dca52e0f1577f1d
MoriKen254 <masamorobo@gmail.com>
2016-12-18 21:51:03 +0900

    Fix dependency.

--- before_script.sh
+++ before_script.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
-sudo apt-get install -y software-properties-common python-software-properties
-yes | sudo apt-add-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl; sudo apt-get update -qq; sudo apt-get install -y libpcl-all;
+pwd
+apt-get install -y software-properties-common python-software-properties
+yes | apt-add-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl; apt-get update -qq; apt-get install -y libpcl-all;
